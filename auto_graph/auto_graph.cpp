6795bc315e2ea65e73782381a529086f67796b1e
Szoke Balazs <balazs.szoke@360.world>
2023-07-06 17:26:31 +0200

    :construction_worker: Fixed more CMake build & debug

--- auto_graph/auto_graph.cpp
+++ auto_graph/auto_graph.cpp
@@ -1,14 +1,50 @@
+// Standard library
 #include <iostream>
 
+// External
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+// auto_graph
 #include "instrument.hpp"
+
+
 
 int main()
 {
-	PROFILE_BEGIN_SESSION("profile", "profile.json");
-	PROFILE_FUNCTION();
-
 	std::cout << "Hello world !" << std::endl;
 
-	PROFILE_END_SESSION();
+	Py_SetPythonHome(CONCAT(L, PYTHON_ROOT_DIR));
+	Py_Initialize();
+
+	PyCompilerFlags cflags = {0};
+	PyThreadState *substate, *mainstate;
+	mainstate = PyThreadState_Get();
+
+	PyThreadState_Swap(NULL);
+
+	const PyInterpreterConfig config = _PyInterpreterConfig_INIT;
+
+	PyStatus status = Py_NewInterpreterFromConfig(&substate, &config);
+	if (PyStatus_Exception(status))
+	{
+		/* Since no new thread state was created, there is no exception to
+		propagate; raise a fresh one after swapping in the old thread
+		state. */
+		PyThreadState_Swap(mainstate);
+		_PyErr_SetFromPyStatus(status);
+		PyObject *exc = PyErr_GetRaisedException();
+		PyErr_SetString(PyExc_RuntimeError, "sub-interpreter creation failed");
+		_PyErr_ChainExceptions1(exc);
+		return NULL;
+	}
+	assert(substate != NULL);
+	auto r = PyRun_SimpleStringFlags("print('Hello')", &cflags);
+	Py_EndInterpreter(substate);
+
+	PyThreadState_Swap(mainstate);
+
+	Py_Finalize();
+
 	return 0;
 }
