33bc496bbb9baf4502eb972e97273238801c28e4
SquareRoundCurly <bala.szoke@gmail.com>
2023-07-19 14:20:59 +0200

    :fire: Removed all the code

--- auto_graph/auto_graph.cpp
+++ auto_graph/auto_graph.cpp
@@ -1,21 +1,11 @@
 // Standard library
+#include <iostream>
 #include <thread>
 #include <chrono>
 using namespace std::chrono_literals;
 
-// auto_graph
-#include "instrument.hpp"
-#include "python_interpreter.hpp"
-
 int main()
 {
-	SRC::AG::Test();
-	// auto pi = SRC::AG::PythonInterpreter();
-	// pi.CreateSubinterpreter();
-	// pi.Run(0, "print(f'Hello {ID}')");
-	// pi.CreateSubinterpreter();
-	// pi.Run(1, "print(f'Hello {ID}')");
-	// pi.Run(0, "print(f'Hello World ! {ID}')");
-
+	std::cout << "Hello world !" << std::endl;
 	return 0;
 }
