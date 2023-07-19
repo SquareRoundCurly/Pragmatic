cb44cf60d4a21e69ee32bb9ab03c93316f7d41d2
Szoke Balazs <balazs.szoke@360.world>
2023-07-19 16:28:09 +0200

    :construction_worker: Build working for extension module

--- auto_graph/auto_graph.cpp
+++ auto_graph/auto_graph.cpp
@@ -4,8 +4,45 @@
 #include <chrono>
 using namespace std::chrono_literals;
 
-int main()
+// External
+#include <Python.h>
+
+extern "C"
 {
-	std::cout << "Hello world !" << std::endl;
-	return 0;
+	int Add(int a, int b) { return a + b; }
+}
+
+static PyObject* add(PyObject* self, PyObject* args)
+{
+	int a;
+	int b;
+
+	if(!PyArg_ParseTuple(args, "ii", &a, &b)) return NULL;
+
+	return Py_BuildValue("i", a + b);
+}
+
+static PyMethodDef methods[] =
+{
+	{ "add",  add, METH_VARARGS, "Add two numbers." },
+	{ NULL, NULL, 0, NULL }
+};
+
+static struct PyModuleDef addmodule =
+{
+	PyModuleDef_HEAD_INIT,
+	"auto_graph_cpp",      // name of module 
+	NULL,                  // module documentation, may be NULL 
+	-1,                    // size of per-interpreter state of the module, or -1 if the module keeps state in global variables. 
+	methods
+};
+
+PyMODINIT_FUNC PyInit_auto_graph_cpp(void)
+{
+	return PyModule_Create(&addmodule);
+}
+
+PyMODINIT_FUNC PyInit_auto_graph_cpp_d(void)
+{
+	return PyModule_Create(&addmodule);
 }
