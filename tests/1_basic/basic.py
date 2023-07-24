4d70f7b65e5aa93dc1c50bacf09e4e362673c368
SquareRoundCurly <bala.szoke@gmail.com>
2023-07-24 16:21:33 +0200

    :construction_worker: Fixed multi phasic module initialization

--- tests/1_basic/basic.py
+++ tests/1_basic/basic.py
@@ -3,17 +3,20 @@ import auto_graph
 
 class BasicTest(unittest.TestCase):
 	def test_0_build(self):
-		try:
-			sleepy_code = """
+		sleepy_code = """
 import threading
 import time
 import random
-# import auto_graph_cpp
+import auto_graph_cpp
 
 time.sleep(random.uniform(0.1, 3))
 print(f"Running on thread {threading.get_ident()}")
 """
-			for i in range(1):
+
+		try:
+			auto_graph.initialize()
+
+			for i in range(20):
 				auto_graph.task(sleepy_code)
 
 			auto_graph.cleanup()
