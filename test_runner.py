8286105527ea613549505459306ce9faadcdb47e
SquareRoundCurly <bala.szoke@gmail.com>
2023-08-16 16:24:01 +0200

    :test_tube: Delayed test runner

--- test_runner.py
+++ test_runner.py
@@ -1,6 +1,32 @@
 import unittest
 import sys
 
+class DelayedTextTestResult(unittest.TextTestResult):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.delayed_output = []
+
+	def addSuccess(self, test):
+		self.delayed_output.append('.')
+
+	def addError(self, test, err):
+		self.delayed_output.append('E')
+
+	def addFailure(self, test, err):
+		self.delayed_output.append('F')
+
+	def addSkip(self, test, reason):
+		self.delayed_output.append('s')
+
+	def printErrors(self):
+		# Print all the delayed outputs at once
+		self.stream.write(''.join(self.delayed_output))
+		self.stream.flush()
+		super().printErrors()
+
+class DelayedTextTestRunner(unittest.TextTestRunner):
+	resultclass = DelayedTextTestResult
+
 if __name__ == "__main__":
 	test_dir = "./tests"
 
@@ -13,5 +39,5 @@ if __name__ == "__main__":
 		suite = loader.discover(test_dir, pattern='*_test.py')
 
 	# Run the tests
-	runner = unittest.TextTestRunner()
+	runner = DelayedTextTestRunner()
 	runner.run(suite)
