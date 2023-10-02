8dd69b4e3e98bc9cc35029a93aa1e82566e5a228
Szoke Balazs <balazs.szoke@360.world>
2023-10-02 12:34:53 +0200

    :wrench: Debug single test cases

--- test_runner.py
+++ test_runner.py
@@ -1,5 +1,31 @@
 import unittest
 import sys
+import os
+import glob
+
+def find_test_module(test_number, base_dir='tests'):
+	# Search for directories that match the pattern
+	matching_dirs = glob.glob(os.path.join(base_dir, f"{test_number}_*"))
+	
+	if not matching_dirs:
+		raise ValueError(f"No matching directory found for {test_number}_* pattern.")
+	if len(matching_dirs) > 1:
+		raise ValueError(f"Multiple directories found for {test_number}_* pattern.")
+	
+	test_dir = matching_dirs[0]
+	
+	# Search for test files that match the pattern within the found directory
+	test_files = glob.glob(os.path.join(test_dir, '*_test.py'))
+	
+	if not test_files:
+		raise ValueError(f"No test file found in directory {test_dir}.")
+	if len(test_files) > 1:
+		raise ValueError(f"Multiple test files found in directory {test_dir}.")
+	
+	# Convert file path to module format
+	module_name = test_files[0].replace(os.sep, '.').replace('.py', '')
+	
+	return module_name
 
 class DelayedTextTestResult(unittest.TextTestResult):
 	def __init__(self, *args, **kwargs):
@@ -34,7 +60,10 @@ if __name__ == "__main__":
 
 	# Run single test or all of them through discovery
 	if len(sys.argv) == 2:
-		suite = loader.loadTestsFromName(sys.argv[1])
+		test_number = int(sys.argv[1])
+		test_name = find_test_module(test_number)
+
+		suite = loader.loadTestsFromName(test_name)
 	else:
 		suite = loader.discover(test_dir, pattern='*_test.py')
 
