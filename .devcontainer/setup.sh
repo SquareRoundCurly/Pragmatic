a722c6226987790317158b1bf2a958f26a2f0989
SquareRoundCurly <bala.szoke@gmail.com>
2023-10-04 12:27:34 +0000

    :whale: Submodule init

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,6 +3,8 @@
 sudo apt-get update
 sudo apt-get install -y git python3
 
+git submodule update --init --recursive
+
 if [ -f "./.devcontainer/first-run-notice.txt" ]; then
 	sudo cp --force ./.devcontainer/first-run-notice.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
 fi
