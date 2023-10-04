0b31b30b6165d1d41bcb98c0b142b6120dc8fd5c
Szoke Balazs <balazs.szoke@360.world>
2023-10-04 12:56:37 +0200

    :whale: Added codespace extensions & installs

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-# apt-get install -y [your dependencies here]
+apt-get install -y git python3
 
 if [ -f "./.devcontainer/welcome-message.txt" ]; then
 	sudo cp --force ./.devcontainer/first-run-notice.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
