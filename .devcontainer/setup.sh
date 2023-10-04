ca6b39ad840e2a203548fdeb4eb9c445db1329a6
Szoke Balazs <balazs.szoke@360.world>
2023-10-04 12:58:35 +0200

    :whale: Fixed welcome message

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,6 +3,6 @@
 apt-get update
 apt-get install -y git python3
 
-if [ -f "./.devcontainer/welcome-message.txt" ]; then
+if [ -f "./.devcontainer/first-run-notice.txt" ]; then
 	sudo cp --force ./.devcontainer/first-run-notice.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
 fi
