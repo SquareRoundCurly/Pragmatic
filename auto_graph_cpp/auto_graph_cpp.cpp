b3174ea26d3b42626eddaa3ffdbd5ff435666c31
SquareRoundCurly <bala.szoke@gmail.com>
2023-10-22 01:09:10 +0200

    :sparkles: Non static module class

--- auto_graph_cpp/auto_graph_cpp.cpp
+++ auto_graph_cpp/auto_graph_cpp.cpp
@@ -7,8 +7,112 @@
 // auto_graph
 #include "Out.hpp"
 
+namespace Pragmatic
+{
+	template<typename T>
+	struct method_traits;
+
+	template<class C, typename R, typename... Args>
+	struct method_traits<R(C::*)(Args...) const>
+	{
+		using class_type = C;
+		using func_type = R(C::*)(Args...);
+	};
+
+	template<class C, typename R, typename... Args>
+	struct method_traits<R(C::*)(Args...)>
+	{
+		using class_type = C;
+		using func_type = R(C::*)(Args...);
+	};
+} // namespace Pragmatic
+
+
+namespace Pragmatic::PythonUtils
+{
+	using namespace Pragmatic::auto_graph;
+
+	template <class ModuleClass>
+	ModuleClass* GetModule()
+	{
+		if (auto_graph_cpp::module == nullptr)
+		{
+			PyModuleDef* moduleDef = &ModuleClass::moduleDef;
+			ModuleClass::module = PyState_FindModule(moduleDef);
+		}
+		return reinterpret_cast<ModuleClass*>(PyModule_GetState(ModuleClass::module));
+	}
+
+	template <class ModuleClass, typename T>
+	ModuleClass* GetModule(T* module)
+	{
+		if (auto_graph_cpp::module == nullptr) ModuleClass::module = (PyObject*)module;
+		return reinterpret_cast<ModuleClass*>(PyModule_GetState((PyObject*)module));
+	}
+
+	template <class ModuleClass>
+	int traverse(PyObject* module, visitproc visit, void* arg)
+	{
+		return GetModule<ModuleClass>(module)->traverse(module, visit, arg);
+	}
+
+	template <class ModuleClass>
+	int clear(PyObject* module)
+	{
+		return GetModule<ModuleClass>(module)->clear(module);
+	}
+
+	template <class ModuleClass>
+	void free(void* module)
+	{
+		GetModule<ModuleClass>(module)->free(module);
+	}
+
+	template <class ModuleClass>
+	int init(PyObject *module)
+	{
+		return GetModule<ModuleClass>(module)->init(module);
+	}
+
+	template <class ModuleClass, PyObject* (ModuleClass::*moduleMethod)(PyObject*, PyObject*)>
+	PyObject* method(PyObject* self, PyObject* args)
+	{
+		ModuleClass* moduleInstance = GetModule<ModuleClass>();
+		return (moduleInstance->*moduleMethod)(self, args);
+	}
+}
+
 namespace Pragmatic::auto_graph
 {
+	static PyMethodDef methods[] =
+	{
+		{ "cleanup", Pragmatic::PythonUtils::method<auto_graph_cpp, &auto_graph_cpp::cleanup>,  METH_NOARGS,  "Cleanup function to be called before exit"                      },
+		{ "reinit",  Pragmatic::PythonUtils::method<auto_graph_cpp, &auto_graph_cpp::reinit>,   METH_NOARGS,  "Callback for module reloads"                                    },
+		{ "print",   Pragmatic::PythonUtils::method<auto_graph_cpp, &auto_graph_cpp::print>,    METH_VARARGS, "A thread safe print function"                                   },
+		{ "task",    Pragmatic::PythonUtils::method<auto_graph_cpp, &auto_graph_cpp::add_task>, METH_VARARGS, "Python files, strings or functions to add to the executor pool" },
+		{ NULL,      NULL,                                                                      0,            NULL                                                             }
+	};
+
+	static PyModuleDef_Slot slots[] =
+	{
+		{ Py_mod_exec, (void*)Pragmatic::PythonUtils::init<auto_graph_cpp> },
+		{ Py_mod_multiple_interpreters, Py_MOD_PER_INTERPRETER_GIL_SUPPORTED },
+		{ 0, NULL }
+	};
+
+	PyModuleDef auto_graph_cpp::moduleDef =
+	{
+		PyModuleDef_HEAD_INIT,
+		"auto_graph_cpp",                                 // name of module 
+		NULL,                                             // module documentation, may be NULL 
+		sizeof(auto_graph_cpp),                           // size of per-interpreter state of the module
+		methods,                                          // methods
+		slots,                                            // slots
+		Pragmatic::PythonUtils::traverse<auto_graph_cpp>, // traverse
+		Pragmatic::PythonUtils::clear<auto_graph_cpp>,    // clear
+		Pragmatic::PythonUtils::free<auto_graph_cpp>      // free
+	};
+
 	PyObject* auto_graph_cpp::cleanup(PyObject* self, PyObject* args)
 	{
 		Out() << "[auto_graph] " << "cleanup" << std::endl;
@@ -38,9 +142,10 @@ namespace Pragmatic::auto_graph
 
 	}
 
-	PyObject* auto_graph_cpp::task(PyObject* self, PyObject* args)
+	PyObject* auto_graph_cpp::add_task(PyObject* self, PyObject* args)
 	{
 		
+
 		Py_RETURN_NONE;
 	}
 
@@ -71,42 +176,13 @@ namespace Pragmatic::auto_graph
 
 		return;
 	}
-
-	static PyMethodDef methods[] =
-	{
-		{ "cleanup", auto_graph_cpp::cleanup, METH_NOARGS,  "Cleanup function to be called before exit"                      },
-		{ "reinit",  auto_graph_cpp::reinit,  METH_NOARGS, "Callback for module reloads"                                     },
-		{ "print",   auto_graph_cpp::print,   METH_VARARGS, "A thread safe print function"                                   },
-		{ "task",    auto_graph_cpp::task,    METH_VARARGS, "Python files, strings or functions to add to the executor pool" },
-		{ NULL,      NULL,                    0,            NULL                                                             }
-	};
-
-	static PyModuleDef_Slot slots[] =
-	{
-		{ Py_mod_exec, (void*)auto_graph_cpp::init },
-		{ Py_mod_multiple_interpreters, Py_MOD_PER_INTERPRETER_GIL_SUPPORTED },
-		{ 0, NULL }
-	};
-
-	static struct PyModuleDef module =
-	{
-		PyModuleDef_HEAD_INIT,
-		"auto_graph_cpp",         // name of module 
-		NULL,                     // module documentation, may be NULL 
-		sizeof(auto_graph_cpp),   // size of per-interpreter state of the module
-		methods,                  // methods
-		slots,                    // slots
-		auto_graph_cpp::traverse, // traverse
-		auto_graph_cpp::clear,    // clear
-		auto_graph_cpp::free,     // free
-	};
 } // namespace Pragmatic::auto_graph
 
 PyMODINIT_FUNC PyInit_auto_graph_cpp(void)
 {
 	using namespace Pragmatic::auto_graph;
 
-    return PyModuleDef_Init(&module);
+    return PyModuleDef_Init(&auto_graph_cpp::moduleDef);
 }
 
 PyMODINIT_FUNC PyInit_auto_graph_cpp_d(void)
--- auto_graph_cpp/auto_graph_cpp.hpp
+++ auto_graph_cpp/auto_graph_cpp.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+// Standard library
+#include <vector> 
+
 // External
 #include <pytypedefs.h>
 
@@ -11,13 +14,20 @@ namespace Pragmatic::auto_graph
 	class auto_graph_cpp
 	{
 		public:
-		static PyObject* cleanup(PyObject* self, PyObject* args);
-		static PyObject* reinit(PyObject* self, PyObject* args);
-		static PyObject* print(PyObject* self, PyObject* args);
-		static PyObject* task(PyObject* self, PyObject* args);
-		static int init(PyObject *module);
-		static int traverse(PyObject* module, visitproc visit, void* arg);
-		static int clear(PyObject* module);
-		static void free(void* module);
+		PyObject* cleanup(PyObject* self, PyObject* args);
+		PyObject* reinit(PyObject* self, PyObject* args);
+		PyObject* print(PyObject* self, PyObject* args);
+		PyObject* add_task(PyObject* self, PyObject* args);
+		int init(PyObject *module);
+		int traverse(PyObject* module, visitproc visit, void* arg);
+		int clear(PyObject* module);
+		void free(void* module);
+
+		public:
+		static PyModuleDef moduleDef;
+		inline static PyObject* module = nullptr;
+
+		private:
+		std::vector<PyObject*> tasks;
 	};
 } // namespace Pragmatic::auto_graph
